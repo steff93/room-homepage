af5e2ca32b6fc5ced27a4fa2b054ecf395290c43
stefan_flo <stefan@flothemes.com>
2023-11-10 23:01:58 +0200

    fixed svg imports and other minor changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import "./App.css";
+import "./App.scss";
 import Header from "./components/Header/Header";
 
 function App() {
   return (
-    <div className="App">
+    <div className="main">
       <Header />
     </div>
   );
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,18 @@
+import { ReactComponent as Logo } from "../../assets/images/logo.svg";
+
 const Header = () => {
   const menuItems = ["home", "shop", "about", "contact"] as const;
 
   return (
-    <header>
-      <nav>
+    <header className="header">
+      <div className="logo">
+        <Logo />
+      </div>
+      <nav className="navigation">
         <ul>
           {menuItems.map((item) => {
             return (
-              <li>
+              <li className="navigation__item">
                 <a href="#">{item}</a>
               </li>
             );
