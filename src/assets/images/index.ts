cf8800c94ff292aab7d5b18c1b4522f825aeaf58
stefan_flo <stefan@flothemes.com>
2023-11-12 19:00:41 +0200

    Added content for all slides

---
+++ src/assets/images/index.ts
@@ -0,0 +1,13 @@
+import SlideImage1 from "./desktop-image-hero-1.jpg";
+import SlideImage2 from "./desktop-image-hero-2.jpg";
+import SlideImage3 from "./desktop-image-hero-3.jpg";
+import FooterImageDark from "./image-about-dark.jpg";
+import FooterImageLight from "./image-about-light.jpg";
+
+export {
+  FooterImageDark,
+  FooterImageLight,
+  SlideImage1,
+  SlideImage2,
+  SlideImage3,
+};
