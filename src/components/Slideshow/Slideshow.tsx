020e803e11d19f04427da24481bd6bcceeb4745b
stefan_flo <stefan@flothemes.com>
2023-11-17 20:16:22 +0200

    Slideshow styling

--- src/components/Slideshow/Slideshow.tsx
+++ src/components/Slideshow/Slideshow.tsx
@@ -1,14 +1,29 @@
 import { Swiper, SwiperSlide } from "swiper/react";
+
+import { Navigation } from "swiper/modules";
 import Slide from "../Slide/Slide";
+import "./Slideshow.scss";
 import { slideshowContent } from "./slideshowContent";
+
+import { ReactComponent as ArrowPrev } from "../../assets/images/icon-angle-left.svg";
+import { ReactComponent as ArrowNext } from "../../assets/images/icon-angle-right.svg";
 
 // Import Swiper styles
 import "swiper/css";
+import "swiper/css/navigation";
 
 const Slideshow = () => {
   return (
     <div className="slideshow-wrapper">
-      <Swiper slidesPerView={1}>
+      <Swiper
+        slidesPerView={1}
+        modules={[Navigation]}
+        loop={true}
+        navigation={{
+          nextEl: ".slideshow-navigation--next",
+          prevEl: ".slideshow-navigation--prev",
+        }}
+      >
         {slideshowContent.map((slide, index) => {
           return (
             <SwiperSlide key={index}>
@@ -17,6 +32,15 @@ const Slideshow = () => {
           );
         })}
       </Swiper>
+
+      <div className="slideshow-navigation">
+        <span className="slideshow-navigation--prev">
+          <ArrowPrev />
+        </span>
+        <span className="slideshow-navigation--next">
+          <ArrowNext />
+        </span>
+      </div>
     </div>
   );
 };
