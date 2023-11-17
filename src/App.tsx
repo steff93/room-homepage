fee40f94d0331be69ccafd739eddf8673e46f3cd
stefan_flo <stefan@flothemes.com>
2023-11-17 20:59:31 +0200

    Added footer component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import "./App.scss";
+import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import Slideshow from "./components/Slideshow/Slideshow";
 
@@ -7,6 +8,7 @@ function App() {
     <div className="main">
       <Header />
       <Slideshow />
+      <Footer />
     </div>
   );
 }
