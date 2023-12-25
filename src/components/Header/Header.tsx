d7755f2728e921ef1d251d013a10b5ccd6052f85
stefan_flo <stefan@flothemes.com>
2023-12-25 22:02:27 +0200

    started work on mobile navigation

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,21 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
+import { ReactComponent as MobileHamburger } from "../../assets/images/icon-hamburger.svg";
 import { ReactComponent as Logo } from "../../assets/images/logo.svg";
 import "./Header.scss";
 
 const Header = () => {
   const menuItems = ["home", "shop", "about", "contact"] as const;
 
+  const handleMobileMenuClick = () => {
+    console.log("click");
+  };
+
   return (
     <header className="header">
+      <MobileHamburger
+        className="mobile-menu"
+        onClick={handleMobileMenuClick}
+      />
       <a className="logo" href="#">
         <Logo />
       </a>
